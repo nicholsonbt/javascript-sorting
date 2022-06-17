5693feb835e771c4e7e44d65217abe0456deb97c
Ben Nicholson <bennicholson777@gmail.com>
2022-06-17 11:18:22 +0100

    Add insertion sort as export

---
+++ sorting.js
@@ -0,0 +1,23 @@
+const insertionSort = (array) => {
+	let insert = function(array, rightIndex, value) {
+		// Search for the first (from the right of index i) index (j) such that the value at j is less than 'value'.
+		// Increment the index of all values between i and j.
+		for (var j = rightIndex; j >= 0 && array[j] > value; j--) {
+			array[j + 1] = array[j];
+		}
+		
+		// Insert 'value' in its sorted location.
+		array[j + 1] = value;
+	}
+	
+	// Iterate (i) from index 1 to N.
+	for (let i = 1; i < array.length; i++) {
+		// Foreach i, compare the element at index i ('value') insert 'value' in its sorted location.
+		insert(array, i-1, array[i]);
+	}
+	
+	// Return the sorted array.
+	return array;
+}
+
+export {insertionSort};
